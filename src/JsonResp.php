14f210b650f4088cca73e16320ad88f6c25a8754
alexcane <12563984+alexcane@users.noreply.github.com>
2024-10-15 16:12:05 +0200

    Add src repository with JsonResp.php class

---
+++ src/JsonResp.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace PhpJsonResp;
+
+class JsonResp
+{
+    private array $_data = [];
+    private string $_status = 'error';
+    private array $_errMsg = [];
+    private ?object $_response;
+
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        $this->setData($data);
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        return $this->_data;
+    }
+
+    /**
+     * Process actions on $data iteration :
+     * - trim() string variable
+     * - true and false string replace as boolean
+     * @param array $data
+     */
+    public function setData(array $data): void
+    {
+        $trimmedData = array();
+        foreach($data as $k=>$v){
+            if(is_string($v)) $v = trim($v);
+            if('false'===$v) $v = false;
+            if('true'===$v) $v = true;
+            $trimmedData[$k] = $v;
+        }
+        $this->_data = $trimmedData;
+    }
+
+    /**
+     * @param string $errMsg
+     */
+    public function addErrMsg(string $errMsg): void
+    {
+        $this->_errMsg[] = $errMsg;
+    }
+
+    /**
+     * @param array $errMsg
+     */
+    public function setErrMsg(array $errMsg): void
+    {
+        $this->_errMsg = $errMsg;
+    }
+
+    /**
+     * Check if response contains error(s)
+     * @return bool
+     */
+    public function isSuccess(): bool
+    {
+        if(empty($this->_errMsg)) return true;
+        return false;
+    }
+
+    /**
+     * @return object
+     */
+    public function getResponse()
+    {
+        return $this->_response;
+    }
+
+    public function setResponse($response): void
+    {
+        $this->_response = $response;
+    }
+
+    /**
+     * @param bool $json
+     * @return array|string
+     */
+    public function returnResponse(bool $json=false)
+    {
+        if(empty($this->_errMsg)) $this->_status = 'success';
+        $res = [
+            'status' => $this->_status,
+            'error_msg' => $this->_errMsg,
+            'data' => $this->_data,
+            'response' => $this->_response,
+        ];
+        if(empty($res['data'])) unset($res['data']);
+        if(empty($res['response'])) unset($res['response']);
+
+        if($json) $res = $this->returnJson($res);
+        return $res;
+    }
+
+    /**
+     * json encode the response
+     * @param array $res
+     * @return string
+     */
+    private function returnJson(array $res): string
+    {
+        return json_encode($res);
+    }
+}
