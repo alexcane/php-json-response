3ac3149aee06ae6b38d63e788bcb2dfcf75f43b5
alexcane <12563984+alexcane@users.noreply.github.com>
2024-10-16 23:18:28 +0200

    Add PHPUnit

---
+++ tests/PasswordValidatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use PhpJsonResp\PasswordValidator;
+use PHPUnit\Framework\TestCase;
+
+class PasswordValidatorTest extends TestCase
+{
+
+    public function testIsValidPassword()
+    {
+        $resp = new PasswordValidator([]);
+
+        //light
+
+        $isValid = $resp->isValidPassword('');
+        $this->assertFalse($isValid, 'password is empty');
+
+        $isValid = $resp->isValidPassword('short');
+        $this->assertFalse($isValid, 'password too short');
+
+        $isValid = $resp->isValidPassword('nodigit');
+        $this->assertFalse($isValid);
+
+        $isValid = $resp->isValidPassword('1digit');
+        $this->assertFalse($isValid);
+
+        //medium
+
+//        $isValid = $resp->isValidPassword('short');
+//        $this->assertFalse($isValid);
+//
+//        $isValid = $resp->isValidPassword('short');
+//        $this->assertFalse($isValid);
+    }
+}
